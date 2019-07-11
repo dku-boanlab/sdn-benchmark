5a8abe231824af18d91667ee10b4a0123e6a6f55
Jaehyun Nam <namjh@kaist.ac.kr>
2019-07-11 07:24:33 +0000

    Update scripts

--- onos/setup.sh
+++ onos/setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Oracle java 8
+
+sudo apt-get install -y wget
+wget -P /tmp/ http://www.sdx4u.net/downloads/jdk-8u202-linux-x64.tar.gz
+
+sudo mkdir -p /usr/lib/jvm
+sudo tar xvfz /tmp/jdk-8u202-linux-x64.tar.gz -C /usr/lib/jvm/
+
+echo "PATH=$PATH:/usr/lib/jvm/jdk1.8.0_202/bin" | sudo tee -a /etc/environment
+echo "JAVA_HOME=/usr/lib/jvm/jdk1.8.0_202/" | sudo tee -a /etc/environment
+echo "CLASSPATH=JAVA_HOME=/usr/lib/jvm/jdk1.8.0_202/lib" | sudo tee -a /etc/environment
+
+. /etc/environment
+
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_202/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_202/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_202/bin/javaws" 1
+
+# ONOS
+
 cp run_onos.sh ~
 
 mkdir ~/Downloads ~/Applications
