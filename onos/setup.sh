39def16911159f81e76909dca05e4c7ec1c2993d
Jaehyun Nam <namjh@kaist.ac.kr>
2019-07-12 21:42:09 +0000

    Update scripts

--- onos/setup.sh
+++ onos/setup.sh
@@ -10,9 +10,10 @@ sudo tar xvfz /tmp/jdk-8u202-linux-x64.tar.gz -C /usr/lib/java/
 
 echo "PATH=$PATH:/usr/lib/java/jdk1.8.0_202/bin" | sudo tee -a /etc/environment
 echo "JAVA_HOME=/usr/lib/java/jdk1.8.0_202/" | sudo tee -a /etc/environment
-echo "JRE_HOME=$JAVA_HOME/jre" | sudo tee -a /etc/environment
+echo "JRE_HOME=/usr/lib/java/jdk1.8.0_202/jre" | sudo tee -a /etc/environment
 
 . /etc/environment
+. ~/.bashrc
 
 sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/java/jdk1.8.0_202/bin/java" 1
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/java/jdk1.8.0_202/bin/javac" 1
